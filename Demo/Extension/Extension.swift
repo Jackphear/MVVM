12c744ada2d2d956e1a4fb15b6bfb87969055bc6
jackphear <1443779045@qq.com>
2021-11-20 17:13:10 +0800

    test

--- Demo/Extension/Extension.swift
+++ Demo/Extension/Extension.swift
@@ -32,6 +32,7 @@ extension UILabel {
     }
     
     func setSize(with text: String) -> CGFloat {
+        print("")
         self.text = text
         let size = sizeThatFits(CGSize(width: 200, height: CGFloat(MAXFLOAT)))
         return size.height + 1
